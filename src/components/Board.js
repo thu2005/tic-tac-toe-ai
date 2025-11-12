db4a8cdb614f17f2e9b1c8a722c893475e0c08fa
thu <thunguyendoanxuan0405@gmail.com>
2025-11-12 16:12:56 +0700

    refactor: rewrite board to use 2 loops

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,24 +1,34 @@
 import Square from "./Square";
 
-function Board({ board, handleClick }) {
+function Board({ board, handleClick, winningLine }) {
+  const renderSquare = (index) => {
     return (
-        <div id="board">
-            <div className="row">
-                <Square value={board[0]} handleClick={() => { handleClick(0) }} />
-                <Square value={board[1]} handleClick={() => { handleClick(1) }} />
-                <Square value={board[2]} handleClick={() => { handleClick(2) }} />
-            </div>
-            <div className="row">
-                <Square value={board[3]} handleClick={() => { handleClick(3)}} />
-                <Square value={board[4]} handleClick={() => { handleClick(4) }} />
-                <Square value={board[5]} handleClick={() => { handleClick(5) }} />
-            </div>
-            <div className="row">
-                <Square value={board[6]} handleClick={() => { handleClick(6) }} />
-                <Square value={board[7]} handleClick={() => { handleClick(7) }} />
-                <Square value={board[8]} handleClick={() => { handleClick(8) }} />
-            </div>
+      <Square
+        key={index}
+        value={board[index]}
+        handleClick={() => handleClick(index)}
+        isWinning={winningLine && winningLine.includes(index)}
+      />
+    );
+  };
+
+  const renderBoard = () => {
+    const rows = [];
+    for (let row = 0; row < 3; row++) {
+      const squares = [];
+      for (let col = 0; col < 3; col++) {
+        const index = row * 3 + col;
+        squares.push(renderSquare(index));
+      }
+      rows.push(
+        <div key={row} className="row">
+          {squares}
         </div>
-    )
+      );
+    }
+    return rows;
+  };
+
+  return <div id="board">{renderBoard()}</div>;
 }
 export default Board;
